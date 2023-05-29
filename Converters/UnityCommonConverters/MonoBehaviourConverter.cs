f03d82f0259c627cd6eed7c8dd46b42c916f4f75
Guilherme Reginaldo Ruella <potentii@gmail.com>
2023-05-28 22:58:25 -0300

    Added monobehaviour converter

---
+++ Converters/UnityCommonConverters/MonoBehaviourConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Potentii.UniLog.Core;
+using UnityEngine;
+
+namespace Potentii.UniLog.Converters.UnityCommonConverters
+{
+    public class MonoBehaviourConverter : IUniLogConverter<MonoBehaviour>
+    {
+        public object Convert(MonoBehaviour value)
+        {
+            if (value == null)
+                return null;
+
+            return new Entry
+            {
+                type = value.GetType().ToString(),
+                isEnabled = value.enabled,
+            };
+        }
+            
+        [Serializable]
+        private struct Entry
+        {
+            public string type;
+            public bool isEnabled;
+        }
+    }
+}
